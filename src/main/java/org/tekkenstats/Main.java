55d0dc8d19be26e9763cacb267bc0f2c253b82df
Cedric <77137812+the-beef-calculator@users.noreply.github.com>
2024-09-20 21:32:33 -0400

    added spring aop depedency for retries

--- src/main/java/org/tekkenstats/Main.java
+++ src/main/java/org/tekkenstats/Main.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableRabbit
-
+@EnableRetry
 public class Main {
     public static void main(String[] args)
     {
