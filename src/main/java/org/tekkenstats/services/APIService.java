e226ce9226554c381bd5fd08956048647d193e73
Cedric <77137812+the-beef-calculator@users.noreply.github.com>
2024-10-09 19:55:13 -0400

    removed unused imports, cleaned up extra spaces in classes

--- src/main/java/org/tekkenstats/services/APIService.java
+++ src/main/java/org/tekkenstats/services/APIService.java
@@ -32,7 +32,7 @@ public class APIService {
     private String API_URL = "https://wank.wavu.wiki/api/replays";
     private static final ZoneId zoneId = ZoneId.of("UTC");
 
-    long unixTimestamp = 1727926739L;
+    long unixTimestamp = 1728495479L;
 
     @Scheduled(fixedRate = 1200)
     public void fetchReplays()
